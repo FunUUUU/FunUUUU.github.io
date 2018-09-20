47b710e6f4b5fe68e808955af709bf036368f638
FunUUUU <wlz15263859383@gmail.com>
2018-09-20 15:36:07 +0800

    QAQ

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 DATE=`date +%Y%m%d_%H%M%S`
+DATE="Updated at $DATE."
 echo "Pushing source to GitHub..."
 git add .
 git commit -m \"$DATE\"
@@ -9,3 +10,4 @@ hexo clean --silent
 echo "Building And Deploying..."
 hexo d -g --silent
 echo "Done!"
+
