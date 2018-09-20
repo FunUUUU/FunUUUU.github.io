dd6d05ebe9bd080ada0d2f9add7d64195d236321
FunUUUU <wlz15263859383@gmail.com>
2018-09-20 15:38:26 +0800

    '20180920_153826'

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 DATE=`date +%Y%m%d_%H%M%S`
-DATE="Updated at $DATE."
 echo "Pushing source to GitHub..."
 git add .
-git commit -m \"$DATE\"
+git commit -m \'$DATE\'
 git push
 echo "Cleaning Cache..."
 hexo clean --silent
